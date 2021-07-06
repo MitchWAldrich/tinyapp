fb12e31e925b338f36a2be806e623b0266ec78c8
Mitchell Aldrich <mitch.w.aldrich@gmail.com>
2021-07-06 19:12:39 +0000

    express_server now redirects to the newly generated shortURL and updates the shortURL: longURL pairs on the /urls page

--- express_server.js
+++ express_server.js
@@ -48,9 +48,9 @@ app.get('/urls/new', (req, res) => {
 
 app.post('/urls', (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  urlDatabase[randomShortURL] = { shortURL: req.body.shortURL, longURL: req.body.longURL }
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
-  res.redirect('/urls')
+  let shortURL = generateRandomString(6);
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get('/urls/:shortURL', (req, res) => {
